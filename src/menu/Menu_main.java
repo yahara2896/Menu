f1c66fe14ac24b3a2e83f65d33d9c4939a1d4cb4
50616046 <50616046@50616046-pc>
2018-01-16 18:21:25 +0900

    1/15課題

---
+++ src/menu/Menu_main.java
@@ -0,0 +1,13 @@
+package menu;
+
+public class Menu_main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Menu_view menu = new Menu_view();
+		menu.setBounds(5,5,655,455);
+		menu.setVisible(true);
+	}
+
+}
